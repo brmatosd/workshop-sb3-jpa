d2c357f63fd21199c95df2b7f72db44cb42ec3df
Daniel Matos <dan.souza.matos@gmail.com>
2023-01-14 23:34:13 -0300

    Config H2 database

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,7 +1,11 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +13,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+@Table(name="tb_user")
 public class User  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	private String name, email, phone, password;
-	
+	private String name;
+	private String email; 
+	private String phone; 
+	private String password;	
 	
 }
