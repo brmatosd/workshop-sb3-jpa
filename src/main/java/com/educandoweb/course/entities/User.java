79d31fa68eb7a530d9806ec80cc0169ab416c8a2
Daniel Matos <dan.souza.matos@gmail.com>
2023-01-24 16:25:49 -0300

    commit 24_01

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,31 +1,59 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
-
-@Data
 @NoArgsConstructor
-@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 @Entity
-@Table(name="tb_user")
-public class User  implements Serializable{
+@Table(name = "tb_user")
+public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
+	@Setter
 	private Long id;
+	@Getter
+	@Setter
 	private String name;
-	private String email; 
-	private String phone; 
+	@Getter
+	@Setter
+	private String email;
+	@Getter
+	@Setter
+	private String phone;
+	@Getter
+	@Setter
 	private String password;
-	
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	@Getter
+	private List<Order> orders = new ArrayList<>();
+
+	public User(Long id, String name, String email, String phone, String password) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+		this.password = password;
+	}
 }
